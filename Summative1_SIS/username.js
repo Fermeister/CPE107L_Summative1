edd6ef806b8e401ed0af82f11419e390738d7213
Gefer Ian Ersan <101352026+Fermeister@users.noreply.github.com>
2022-03-31 03:43:15 +0800

    Add files via upload

---
+++ Summative1_SIS/username.js
@@ -0,0 +1,31 @@
+function validate(){
+
+    var user=document.getElementById("user").value;
+    var pass=document.getElementById("pass").value;
+    var usertype=document.getElementById("usertype").value;
+
+    if (user=="admin" && pass=="admin" && usertype =="admin")
+    {
+        window.location.href="admin.html";
+    }
+    else if(user=="student1" && pass=="1234" && usertype =="student")
+    {
+        window.location.href="student.html";
+    }
+
+    else if(user=="teacher1" && pass=="1234" && usertype =="student")
+    {
+        window.location.href="teacher.html";
+    }
+
+    else if(user=="" && pass=="")
+    {
+        alert("please enter your username and password");
+    }
+
+    else
+    {
+        alert("You have entered a wrong username and password");
+    }
+
+}
